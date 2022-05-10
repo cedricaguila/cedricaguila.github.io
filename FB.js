29f3b143b9e4759d3c564ede59eaad8f958e6ec9
Cedric <cedricaguila.taktyl@gmail.com>
2022-05-10 10:03:35 +0800

    Local image testing

--- FB.js
+++ FB.js
@@ -1,5 +1,9 @@
 document.body.appendChild(Object.assign(document.createElement("script"), { type: "text/javascript", src: "https://connect.facebook.net/en_US/sdk.js", defer: true }))
 
+window.facebook = {
+    share
+}
+
 window.fbAsyncInit = function() {
     FB.init({
       appId            : '360130579428677',
@@ -9,9 +13,23 @@ window.fbAsyncInit = function() {
     });
 }
 
-share = function() {
+function shareone() {
     FB.ui({
       method: 'share',
-      href: 'cedricaguila.github.io'
+      href: 'cedricaguila.github.io/one.html'
+    }, function(response) {});
+}
+
+function sharetwo() {
+    FB.ui({
+      method: 'share',
+      href: 'cedricaguila.github.io/two.html'
+    }, function(response) {});
+}
+
+function sharethree() {
+    FB.ui({
+      method: 'share',
+      href: 'cedricaguila.github.io/three.html'
     }, function(response) {});
 }
